d652f48b8213d0969acffd2fd5f198b5a1f78ded
Kevin Adrian <kevin19305.ib@gmail.com>
2025-10-17 16:46:30 +0700

    Spacer: Untuk membuat jarak atau spasi kosong vertikal setinggi 10.dp.
    Box: Untuk membuat sebuah wadah (kotak) berwarna biru muda (cyan) dengan lebar penuh dan tinggi 300.dp, di mana semua isi di dalamnya akan diletakkan tepat di tengah.

--- app/src/main/java/com/example/questbasiccomposable_027/tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_027/tataletak.kt
@@ -131,6 +131,14 @@ fun tataletakBoxColumnRow(modifier: Modifier) {
                 }
             }
         }
+        Spacer(modifier = Modifier.height(height = 10.dp))
+        Box(
+            modifier = modifier
+                .fillMaxWidth()
+                .height(height = 300.dp)
+                .background(color = Color.Cyan),
+            contentAlignment = Alignment.Center
+        ){
 
         }
 

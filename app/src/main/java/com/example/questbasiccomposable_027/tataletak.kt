966ff2f9b7a1ac4bfa0bfff42756898c755ecf53
Kevin Adrian <kevin19305.ib@gmail.com>
2025-10-17 16:33:35 +0700

    Membuat tataletak Rows

--- app/src/main/java/com/example/questbasiccomposable_027/tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_027/tataletak.kt
@@ -1,18 +1,28 @@
 package com.example.questbasiccomposable_027
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.LineHeightStyle
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 
 @Composable
@@ -25,5 +35,13 @@ fun tataletakColumn(modifier: Modifier) {
     }
 }
 
-
-
+@Composable
+fun tataletakRow(modifier: Modifier) {
+    Row (modifier = modifier.fillMaxWidth(),
+        horizontalArrangement = Arrangement.SpaceEvenly) {
+        Text(text = "Komponen1")
+        Text(text = "Komponen2")
+        Text(text = "Komponen3")
+        Text(text = "Komponen4")
+    }
+}

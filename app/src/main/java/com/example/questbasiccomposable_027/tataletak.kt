bb70954d941781923124a97f14e0b489979615cc
Kevin Adrian <kevin19305.ib@gmail.com>
2025-10-17 16:49:02 +0700

    untuk menampilkan teks "My Kampus" dengan gaya yang sangat mencolok: ukurannya sangat besar (50.sp), berwarna merah, tebal, dan menggunakan font gaya tulisan sambung (kursif). Teks ini juga diposisikan tepat di tengah wadahnya.

--- app/src/main/java/com/example/questbasiccomposable_027/tataletak.kt
+++ app/src/main/java/com/example/questbasiccomposable_027/tataletak.kt
@@ -142,7 +142,13 @@ fun tataletakBoxColumnRow(modifier: Modifier) {
             Image(painter = gambar,
                 contentDescription = null,
                 contentScale = ContentScale.Fit)
-
+            Text(text = "My Kampus",
+                fontSize = 50.sp,
+                color = Color.Red,
+                fontWeight = FontWeight.Bold,
+                fontFamily = FontFamily.Cursive,
+                modifier = Modifier.align(
+                    alignment = Alignment.Center))
         }
 
     }
